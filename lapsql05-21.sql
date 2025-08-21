9cdcfb6bbdd2a253e879e9c6b268ebc5d5678d6a
thehorseisgreen <kookv30122548@gmail.com>
2025-08-21 13:53:33 +0700

    Add files via upload

--- lapsql05-21.sql
+++ lapsql05-21.sql
@@ -1,3 +1,19 @@
-SELECT CategoryName, ProductName, UnitPrice
-From Products, Categories
-Where Products.CategoryId=Categories.CategoryID
+--แสดงชื่อประเภทสินค้า ชื่อสินค้า และ ราคาสินค้า
+SELECT c.CategoryName, p.ProductName, p.UnitPrice
+From Products AS p, Categories as c
+Where CategoryName = 'seafood'
+
+SELECT c.CategoryName, p.ProductName, p.UnitPrice
+From Products AS p join, Categories as c
+on p.CategoryID= c.CategoryID
+Where CategoryName = 'seafood'
+
+--แสดงชื่อประเภทสินค้า ชื่อสินค้า และ ราคาสินค้า
+SELECT companyName, OrderId
+From Orders, Shippers
+WHERE Shippers.ShipperID = Orders.ShipVia
+
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID=Orders.Shipvia
+WHERE OrderID=10275
